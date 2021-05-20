1f4871fa008525d752fe64b26086242588b3e88a
feng19 <feng_19@foxmail.com>
2021-05-20 16:25:02 +0800

    deps: update deps

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule SpiderManUi.MixProject do
     [
       {:phoenix, "~> 1.5.8"},
       {:phoenix_live_view, "~> 0.15.1"},
-      {:floki, ">= 0.27.0", only: :test},
+      {:floki, "~> 0.30"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
